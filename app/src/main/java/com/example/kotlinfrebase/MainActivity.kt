2eaf61d49466c3976e5cec8a7ad09d1dcdeadad3
tuan <tranthanhtuan199981@gmail.com>
2022-11-16 11:15:44 +0900

    チャンタントゥアン登録機能成功

--- app/src/main/java/com/example/kotlinfrebase/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfrebase/MainActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.collections.List
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mAuth: FirebaseAuth
@@ -20,27 +24,43 @@ class MainActivity : AppCompatActivity() {
 
         mAuth = FirebaseAuth.getInstance()
 
-        button.setOnClickListener(View.OnClickListener {
-            mAuth.createUserWithEmailAndPassword(editTextTextPersonName.text.toString().trim(),editTextTextPersonName3.text.toString().trim()).addOnCompleteListener { task->
 
-                if (task.isSuccessful){
-                    FirebaseUserID = mAuth.currentUser!!.uid
-                    refUsers = FirebaseDatabase.getInstance().reference.child("Users").child(FirebaseUserID)
+        var inputEmail: EditText =findViewById(R.id.inputEmail)
+        var inputPassword: EditText =findViewById(R.id.inputPassword)
+        val btnLogin: Button =findViewById(R.id.btnLogin)
+        val gotoRegister: TextView =findViewById(R.id.gotoRegister)
 
-                    val userHasMap = HashMap<String, Any>()
-                    userHasMap["uid"] = FirebaseUserID
-                    userHasMap["email"] = editTextTextPersonName.text.toString().trim()
-                    userHasMap["password"] =editTextTextPersonName3.text.toString().trim()
-                    refUsers.updateChildren(userHasMap)
-                        .addOnCompleteListener{task ->
-                            if (task.isSuccessful){
-                                Toast.makeText(this,"登録成功", Toast.LENGTH_LONG).show()
-                            }
-                        }
-                }
-            }
+
+        //login機能
+        btnLogin.setOnClickListener(View.OnClickListener {
+            loginHandle()
         })
 
+        //新規登録機能
+        gotoRegister.setOnClickListener(View.OnClickListener {
+            signUp()
+        })
+    }
+    fun loginHandle() {
+        var email: String = inputEmail.text.toString()
+        var password: String = inputPassword.text.toString()
+
+        if (email == "admin@gmail.com" && password == "123456") {
+
+            Toast.makeText(this, "ログイン成功", Toast.LENGTH_LONG).show()
+            val intent = Intent(this, List::class.java)
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "ログイン失敗", Toast.LENGTH_LONG).show()
+        }
+    }
+    fun signUp(){
+        val intern = Intent(this,Register::class.java)
+        startActivity(intern)
+    }
+
+
+    fun new(){
 
     }
 }
--- app/src/main/java/com/example/kotlinfrebase/Register.kt
+++ app/src/main/java/com/example/kotlinfrebase/Register.kt
@@ -1,11 +1,130 @@
 package com.example.kotlinfrebase
 
+import android.content.ContentValues
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_register.*
 
 class Register : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var refUsers: DatabaseReference
+    private var FirebaseUserID: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        AlreadyHaveAccount.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+        })
+        btnRegister.setOnClickListener(View.OnClickListener
+        {
+
+            check(inputEmail.text.toString().trim(),InputConfirmEmail.text.toString().trim(),
+                inputPasswords.text.toString().trim(),InputConfirmPassword.text.toString().trim(),)
+        })
+
+
+    }
+
+    private fun check(trim: String, trim1: String, trim2: String, trim3: String) {
+        var checkEmail: Boolean = false
+        var checkPassword: Boolean = false
+        if (trim.isEmpty()||trim1.isEmpty()){
+            Toast.makeText(this,"メールエラー",Toast.LENGTH_LONG).show()
+            inputEmail.setHintTextColor(resources.getColor(android.R.color.holo_red_dark))
+            InputConfirmEmail.setHintTextColor(resources.getColor(android.R.color.holo_red_dark))
+//           inputEmail.setTextColor(resources.getColor(android.R.color.holo_red_dark))
+            checkEmail = false
+        }else{
+
+            if (trim.equals(trim1)){
+                InputConfirmEmail.setTextColor(resources.getColor(R.color.black))
+                inputEmail.setTextColor(resources.getColor(R.color.black))
+                checkEmail = true
+            }else{
+                InputConfirmEmail.setTextColor(resources.getColor(android.R.color.holo_red_dark))
+                inputEmail.setTextColor(resources.getColor(android.R.color.holo_red_dark))
+
+                checkEmail = false
+            }
+        }
+
+        if (trim2.isEmpty()||trim3.isEmpty()){
+            Toast.makeText(this,"パスワードエラー",Toast.LENGTH_LONG).show()
+            inputPasswords.setHintTextColor(resources.getColor(android.R.color.holo_red_dark))
+            InputConfirmPassword.setHintTextColor(resources.getColor(android.R.color.holo_red_dark))
+            checkPassword = false
+        }else{
+
+            if (trim2.equals(trim3)){
+                inputPasswords.setTextColor(resources.getColor(R.color.black))
+                InputConfirmPassword.setTextColor(resources.getColor(R.color.black))
+                checkPassword = true
+            }else{
+                inputPasswords.setTextColor(resources.getColor(android.R.color.holo_red_dark))
+                InputConfirmPassword.setTextColor(resources.getColor(android.R.color.holo_red_dark))
+                checkPassword = false
+            }
+        }
+        if (checkEmail && checkPassword){
+
+
+            mAuth.createUserWithEmailAndPassword(trim,trim2).addOnCompleteListener { task->
+
+                if (task.isSuccessful){
+                    FirebaseUserID = mAuth.currentUser!!.uid
+                    refUsers = FirebaseDatabase.getInstance().reference.child("Users").child(FirebaseUserID)
+
+                    val userHasMap = HashMap<String, Any>()
+                    userHasMap["uid"] = FirebaseUserID
+                    userHasMap["email"] = trim
+                    userHasMap["password"] =trim2
+                    refUsers.updateChildren(userHasMap)
+                        .addOnCompleteListener{task ->
+                            if (task.isSuccessful){
+                                Toast.makeText(this,"登録成功", Toast.LENGTH_LONG).show()
+                                startActivity(Intent(this,MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK))
+                                finish()
+                            }
+                        }
+                }else{
+                    Log.d(ContentValues.TAG, "check22: "+task.exception!!.message.toString())
+                    Toast.makeText(this,"エラー：" + task.exception!!.message.toString(),Toast.LENGTH_LONG).show()
+                }
+            }
+
+//            mAuth.createUserWithEmailAndPassword(trim,trim2).addOnCompleteListener{task ->
+//                if (task.isSuccessful){
+//                    FirebaseUserID = mAuth.currentUser!!.uid
+//                    refUsers = FirebaseDatabase.getInstance().reference.child("Users").child(FirebaseUserID)
+//
+//                    val userHasMap = HashMap<String, Any>()
+//                    userHasMap["uid"] = FirebaseUserID
+//                    userHasMap["email"] = trim
+//                    userHasMap["password"] = trim2
+//                    refUsers.updateChildren(userHasMap)
+//                        .addOnCompleteListener{task ->
+//                            if (task.isSuccessful){
+//                                Toast.makeText(this,"登録成功",Toast.LENGTH_LONG).show()
+//
+//                            }
+//                        }
+//
+//                    //hai
+//                }
+//            }
+
+        }
+
     }
 }
