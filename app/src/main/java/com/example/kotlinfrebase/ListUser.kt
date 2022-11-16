85b9453b0a97150eb4ac28f4e6064813a12a03ac
tuan <tranthanhtuan199981@gmail.com>
2022-11-16 14:25:23 +0900

    チャンタントゥアン　ログイン　ログアウト　登録　エラーなし

--- app/src/main/java/com/example/kotlinfrebase/ListUser.kt
+++ app/src/main/java/com/example/kotlinfrebase/ListUser.kt
@@ -1,11 +1,23 @@
 package com.example.kotlinfrebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.aa.*
 
 class ListUser : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+        btnLogOut.setOnClickListener(View.OnClickListener {
+
+            FirebaseAuth.getInstance().signOut()
+            Toast.makeText(this, "ログイン成功", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        })
     }
 }
